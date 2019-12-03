54c2a740ce1665af91e847d919f48de486f2aba5
jdempsey <Jason.Dempsey@microsoft.com>
2019-12-03 08:11:31 -0500

    First, with part 1 completed

---
+++ day1.lisp
@@ -0,0 +1,17 @@
+(defun part1 ()
+  (with-open-file (stream "C:\\Users\\jadempse\\Documents\\Projects\\aoc19\\input-1.txt")
+    (do ((line (read-line stream) (read-line stream nil 'eof))
+	 (mass 0))
+	((eq line 'eof) mass)
+      (setf mass (+ mass (- (floor (/ (parse-integer line) 3)) 2))))))
+
+
+(defun part2 ()
+  (labels ((fuel-eq (x)
+	     (- (floor (/ (parse-integer x) 3)) 2)))
+    (with-open-file (stream "C:\\Users\\jadempse\\Documents\\Projects\\aoc19\\input-1.txt")
+      (do ((line (read-line stream) (read-line stream nil 'eof))
+	   (mass 0))
+	  ((eq line 'eof) mass)
+	(setf mass (+ mass (- (floor (/ (parse-integer line) 3)) 2))))))
+  
